00abbc25b684901690ed5fb5199aca46ca8f5acc
Dimple choudhary <96401557+Dimple-Choudhary@users.noreply.github.com>
2023-01-30 10:11:29 +0530

    Add files via upload

---
+++ React projects/business card/co1404f828acf487d26d8781c/components/Info.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Info(){
+    return (
+    <main className="stick">
+          <img className="photo" src="./images/photo.png" width="300" height="300"/>
+          <h1 className="Name">Dimple Choudhary</h1>
+          <h3 className="Profession">Frontend Developer</h3>
+          <a  href="mailto:dimplechoudhary1606@gmail.com"><button className="button-email"><img className="img-LinkedIn1" src="./images/envelope-solid.svg" />Email</button>
+          </a>
+          <a href="https://www.linkedin.com/in/dimple-choudhary--/"><button type="Sumbit" className="button-Linkedin" ><img className="img-LinkedIn1" src="./images/linkedin.svg" /> LinkedIn</button>
+          </a>
+        
+  </main>
+    )
+}
+//https://www.linkedin.com/in/dimple-choudhary--/
+//mailto:email@example.com
